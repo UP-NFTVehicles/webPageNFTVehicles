34916e45035dffae05c1ecbda15097aa852c7d83
woryx7 <miguel.herrerap@liceodelvalle.edu.mx>
2022-07-30 10:19:41 -0600

    Fixed hitbox problem, added only one card visible functionality

--- script.js
+++ script.js
@@ -1,38 +1,18 @@
-
-/* var card_array = document.getElementsByClassName("card__paragraph")
-alert(card_array[0].innerHTML) */
-
-// let a = document.getElementsByClassName("card__paragraph");
-// let b = Object.values(a)
-
-// var result = Object.keys(a).map((key) => [Number(key), a[key]]);
-
-// console.log(result);
+let last_modification;
 
 
 const clicked1 = (ev) => {
-    ev.target.querySelector('p').style.display = "Block";
+    console.log(ev.target.tagName)
+    var element = ev.target
+    element.parentElement.querySelector('p').style.display = "Block";
+    if(last_modification){
+      last_modification.parentElement.querySelector('p').style.display = "None";
+    }
+    last_modification = ev.target
+
 }
 
-const clicked2 = (ev) => {
-    ev.target.parentElement.querySelector('p').style.display = "Block";
-}
-
-const clicked3 = (ev) => {
-    ev.target.style.display = "Block";
-}
-
-const collection = document.getElementsByClassName("second__pane__cards");
+const collection = document.getElementsByClassName("clickbox");
 for (let i = 0; i < collection.length; i++) {
   collection[i].addEventListener("click", clicked1);
-}
-
-const collection2 = document.getElementsByClassName("card__svg");
-for (let i = 0; i < collection2.length; i++) {
-  collection2[i].addEventListener("click", clicked2);
-}
-
-const collection4 = document.getElementsByClassName("card__title");
-for (let i = 0; i < collection.length; i++) {
-  collection[i].addEventListener("click", clicked2);
 }
